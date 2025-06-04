4d5e3123c38c8888b0cc6432c84de760ac757110
Vishruti24 <vishu2412004@gmail.com>
2025-06-04 18:16:31 +0530

    commit1

---
+++ src/components/News.js
@@ -0,0 +1,101 @@
+import React, { Component } from 'react'
+
+import NewsItems from './NewsItems'
+//import Spinner from './Spinner'
+import PropTypes from 'prop-types'
+//import InfiniteScroll from "react-infinite-scroll-component"
+
+
+
+export class News extends Component {
+  static defaultProps = {
+    country:'us',
+    pageSize:8,
+    category:"science"
+  }
+  static propTypes={
+    country:PropTypes.string,
+    pageSize:PropTypes.number,
+    category:PropTypes.string,
+
+  }
+    constructor(){
+    super();
+    //console.log("hello i'm constructor from news components");
+    this.state={
+        articles:[],
+        loading:false,
+        page:1,
+    }
+     
+   } 
+   async updateNews(){
+        const url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=fcae3d36684547ec90820ea09d27b60a&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
+        let data=await fetch(url);
+        let parsedData=await data.json();
+        //console.log(parsedData);
+        this.setState({
+          articles:parsedData.articles,
+          totalResults:parsedData.totalResults,
+        })
+   }
+   async componentDidMount(){
+       this.updateNews(); 
+       document.title = `${this.props.category} -NewsApp`;     
+    }
+  //  fetchMoreData = () => {
+  //   this.setState({page:this.state.page+1});
+  //   this.updateNews()
+  // };
+     handlePreClick= async ()=>{
+       console.log("pre");
+       this.setState({page:this.state.page-1});
+       this.updateNews(); //function call
+    }
+     handleNextClick= async ()=>{
+     this.setState({page:this.state.page+1});
+     this.updateNews();//function call
+    }
+  render() {
+    //console.log("render");
+    return (
+      <div className="container my-3">
+        <h1 className='text-center' style={{margin:'35px 0px'}}>NewsApp -Top Headlines From {this.props.category} </h1>
+        {/* {!this.state.loading && <Spinner/>} */}
+
+        {/* <InfiniteScroll
+          dataLength={this.state.articles?.length || 0}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        > */}
+       <div className="row">
+        {this.state.articles.map((element)=>{
+             return <div className="col-md-4"  key={element.url}>
+        <NewsItems title={element.title||""} discription={element.description||""} imageurl={element.urlToImage} newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+        </div>
+        })
+       }
+       </div>
+       {/* </InfiniteScroll> */}
+        <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreClick}>&larr;Previous</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
+        {/* <div className="col-md-4">
+        <NewsItems title="myTitle" discription="mydesc"/>
+        </div>
+        <div className="col-md-4">
+        <NewsItems title="myTitle" discription="mydesc"/>
+        </div> */}
+        {/* </div> */}
+        {/* <NewsItems/>
+        <NewsItems/>
+        <NewsItems/> */}
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/components/NewsItems.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+   
+
+  render() {
+    let {title,discription,imageurl,newsURL,author,date,source}=this.props;//destructing in js
+    return (
+      <div className='my-3'>
+        <div className="card">
+          <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"> {source}</span>
+          <img src={!imageurl?"https://images-prod.gothamist.com/images/Getty.2e16d0ba.fill-1200x650.format-webp.webpquality-85_TJWAHn4.webp":imageurl} className="card-img-top" alt="..."/>
+          <div className="card-body">
+               <h5 className="card-title">{title}</h5>
+               <p className="card-text">{discription}</p>
+               <p className="card-text text-primary"><small className='text-primary'>By{!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+               <a rel="noreferrer" href={newsURL} target="_blank" className="btn btn-sm btn-outline-dark">Read More</a>    
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default NewsItems
